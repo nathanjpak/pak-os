fc14ba855e6845f9561ceac375c2abf9dd22171b
nathanjpak <nathan.j.pak@gmail.com>
2023-04-22 19:03:14 -0700

    refactor drag and resize with state

--- src/components/Desktop/Window/WindowHandle.tsx
+++ src/components/Desktop/Window/WindowHandle.tsx
@@ -1,28 +1,50 @@
-import { useContext, useState } from "react";
+import { Dispatch, SetStateAction, useContext, useState } from "react";
 import WindowsContext from "../../../contexts/windowsContext";
+import { Position } from "react-rnd";
+import { Dimension } from ".";
 
 interface IWindowHandleProps {
   fileName: string;
+  setPosition: Dispatch<SetStateAction<Position>>;
+  setSize: Dispatch<SetStateAction<Dimension>>;
+  parentWidth: number;
+  parentHeight: number;
 }
 
-export const WindowHandle = ({ fileName }: IWindowHandleProps) => {
+export const WindowHandle = ({
+  fileName,
+  setPosition,
+  setSize,
+  parentWidth,
+  parentHeight,
+}: IWindowHandleProps) => {
   const classNameString = `handle relative align-middle text-center py-1 bg-dark-navy text-slate-50 rounded-t`;
   const [isFullScreen, setIsFullScreen] = useState(false);
+  // const [prevSize, setPrevSize] = useState<Dimension>({ width: 0, height: 0 });
+  const { removeOpenedWindow } = useContext(WindowsContext);
 
   const toggleIsFullScreen = () => setIsFullScreen(!isFullScreen);
 
-  const { removeOpenedWindow } = useContext(WindowsContext);
+  const handleResizeClick = () => {
+    toggleIsFullScreen();
+    if (!isFullScreen) {
+      setPosition({ x: 0, y: 0 });
+      setSize({ width: parentWidth, height: parentHeight });
+    } else {
+      setSize({ width: 100, height: 100 });
+    }
+  };
 
   return (
     <div className={classNameString}>
-      {fileName}
+      {`${parentWidth} : ${parentHeight}`}
       <div className="absolute end-1 top-px">
         <button className="rounded-full w-6 h-6 hover:bg-white/50">
           {"\u005f"}
         </button>
         <button
           className="m-1 rounded-full w-6 h-6 hover:bg-white/50"
-          onClick={toggleIsFullScreen}
+          onClick={handleResizeClick}
         >
           {isFullScreen ? "\u29c9" : "\u2610"}
         </button>
--- src/components/Desktop/Window/index.tsx
+++ src/components/Desktop/Window/index.tsx
@@ -1,9 +1,31 @@
-import { Rnd } from "react-rnd";
+import { Position, Rnd, RndResizeCallback } from "react-rnd";
 import { WindowHandle } from "./WindowHandle";
+import { useState } from "react";
+
+export type Dimension = {
+  width: number;
+  height: number;
+};
 
 export const Window = ({ fileName }: { fileName: string }) => {
   const parentWidth = parent.innerWidth;
   const parentHeight = parent.innerHeight;
+
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+  const [size, setSize] = useState<Dimension>({
+    width: parentWidth / 2,
+    height: parentHeight / 2,
+  });
+
+  const handleResize: RndResizeCallback = (e, dir, ref, delta, pos) => {
+    const newSize: Dimension = {
+      width: size.width + delta.width,
+      height: size.height + delta.height,
+    };
+
+    setSize(newSize);
+    setPosition(pos);
+  };
 
   // const dotIndex = fileName.lastIndexOf(".");
   // // This does not account for folder with . in the name
@@ -11,13 +33,47 @@ export const Window = ({ fileName }: { fileName: string }) => {
 
   return (
     <Rnd
-      default={{ x: 0, y: 0, height: parentHeight / 2, width: parentWidth / 2 }}
+      default={{
+        x: position.x,
+        y: position.y,
+        height: size.height,
+        width: size.width,
+      }}
+      size={{
+        width: size.width,
+        height: size.height,
+      }}
+      position={{
+        x: position.x,
+        y: position.y,
+      }}
+      onDragStop={(e, d) => {
+        setPosition({
+          x: d.x,
+          y: d.y,
+        });
+      }}
+      onResizeStop={handleResize}
+      maxWidth={parentWidth}
+      maxHeight={parentHeight}
       bounds="parent"
       dragHandleClassName="handle"
       className="bg-white rounded-t"
     >
-      <WindowHandle fileName={fileName} />
+      <WindowHandle
+        fileName={fileName}
+        setPosition={setPosition}
+        setSize={setSize}
+        parentWidth={parentWidth}
+        parentHeight={parentHeight}
+      />
       Draggable {fileName}
+      <p>
+        {position.x}, {position.y}
+      </p>
+      <p>
+        {size.width}, {size.height}
+      </p>
     </Rnd>
   );
 };
